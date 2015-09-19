bd401bf3c0257cd0880d14b0e601905cbd57a0c9
ankurpawar <ankurpawar19@gmail.com>
2015-09-19 08:04:01 +0530

    Added readme file and minor correction in app.c.

--- simple_modules/misc_driver/app.c
+++ simple_modules/misc_driver/app.c
@@ -9,14 +9,14 @@ int main()
 {
 	int ret;
 	int fd;
-	int len = strlen(ID);
+	int len = strlen(MAGIC_NUM);
 	fd = open("/dev/misc_drv",O_WRONLY);
 	if (fd < 0)
 	{
 		perror("error in opening file\n");
 		exit(EXIT_FAILURE);
 	}
-	ret = write(fd, ID, len);
+	ret = write(fd, MAGIC_NUM, len);
 	printf("ret = %d\n", ret);
 	return 0;
 }
