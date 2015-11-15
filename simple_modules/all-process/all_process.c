53c51288c1e9db7f5e81fd2f510259e8c7bfb8fa
ankurpawar <ankurpawar19@gmail.com>
2015-11-15 10:28:10 +0530

    Corrections to make code work and code formatting.

--- simple_modules/all-process/all_process.c
+++ simple_modules/all-process/all_process.c
@@ -7,112 +7,104 @@
 
 #define PROC_NAME "readme"
 
-static struct proc_dir_entry *entry=NULL; 
+static struct proc_dir_entry *proc_entry = NULL; 
 
-//struct task_struct init_task;
-
-static void *my_seq_start(struct seq_file *seq,loff_t *pos)
+static void *my_seq_start(struct seq_file *seq, loff_t *pos)
 {
-   struct task_struct *task;	
-   loff_t off = 0;
+	struct task_struct *task;
+	loff_t off = 0;
 
-
-   for_each_process(task)
-   {
-      if(*pos == off++)   
-      {
-           printk("in start pos=%d\n",*pos);
-	   return task;   
-      }	      
-  
-   }	   
-
-   printk("start seq over\n");
-   return NULL;
+	for_each_process (task) {
+		if(*pos == off++) {
+			printk("in start pos=%u\n", (unsigned int)off);
+			return task;   
+		}
+	}
+	printk("start seq over\n");
+	return NULL;
 }
 
-
-static void *my_seq_next(struct seq_file *seq,void *v,loff_t *pos)
+static void *my_seq_next(struct seq_file *seq, void *v, loff_t *pos)
 {
-    struct task_struct *p=v,*n;	
-    n=next_task(p);
-
-    ++*pos;
-    printk("in seq_next pos=%d\n",*pos);
-
-    return (n != &init_task)?next_task(n):NULL;    
+	struct task_struct *p=v;	
+	struct task_struct *n;
+	n = next_task(p);
+	
+	++*pos;
+	printk("in seq_next pos=%u\n",(unsigned int)*pos);
+	return (n != &init_task) ? next_task(n) : NULL;    
 }
 
-static int my_seq_show(struct seq_file *seq,void *v)
+static int my_seq_show(struct seq_file *seq, void *v)
 {
-   int ret;
-   struct task_struct *r,*p;
-   int num_thread=0;
-   r=v;
-   p=v;   
+	int ret;
+	struct task_struct *p;
+	struct task_struct *r;
+	int num_thread = 0;
+	r = (struct task_struct*) v;
+	p = (struct task_struct*) v;
+	
+	do {
+		num_thread++;
+	} while_each_thread(p, r);
 
+	ret = seq_printf(seq, "name = %s," 
+				"pid = %d ," 
+				"tgid = %d ,"
+				"stack addr = %d ," 
+				"num threads = %d \n",
+				p->comm,
+				p->pid,
+				p->tgid,
+				p->stack+THREAD_SIZE,
+				num_thread);
 
-   do{
-       num_thread++;
-   }while_each_thread(p,r);
-      
-   ret=seq_printf(seq,"name = %s , pid = %d , tgid = %d , stack addr = %d , num threads = %d \n"
-		         ,p->comm,p->pid,p->tgid,p->stack+THREAD_SIZE,num_thread);
-
-   printk("seq_printf returned %d",ret);
-   
-   return 0;
+	printk("seq_printf returned %d", ret);
+	return 0;
 }
 
-
-static void my_seq_stop(struct seq_file *seq,void *v)
+static void my_seq_stop(struct seq_file *seq, void *v)
 {
-   printk("in stop function\n");
+	printk("in stop function\n");
 }
-
 
 static struct seq_operations my_seq_ops = {
-    .start = my_seq_start,
-    .next  = my_seq_next,
-    .show  = my_seq_show,
-    .stop  = my_seq_stop 
+	.start = my_seq_start,
+	.next  = my_seq_next,
+	.show  = my_seq_show,
+	.stop  = my_seq_stop
 };
 
-static int my_seq_open(struct inode *inode,struct file *file)
+static int my_seq_open(struct inode *inode, struct file *file)
 {
-    printk("in my_seq_open\n");
-    return seq_open(file,&my_seq_ops); 
+	printk("in my_seq_open\n");
+	return seq_open(file, &my_seq_ops); 
 }
 
-static struct file_operations my_file_ops = {
-    .owner = THIS_MODULE ,
-    .open  = my_seq_open ,
-    .read  = seq_read ,
-    .llseek = seq_lseek ,  
-    .release = seq_release
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open  = my_seq_open,
+	.read  = seq_read,
+	.llseek = seq_lseek,
+	.release = seq_release
 };
 
 static int __init procex_init(void)
 {
-   entry = create_proc_entry(PROC_NAME,S_IRUSR,NULL);
-   if(entry)
-   {
-      entry->proc_fops = &my_file_ops;
-   }
-   else
-   {
-      return -EINVAL;
-   }	   
-
-   return 0;
+	proc_entry = proc_create(PROC_NAME, 0, NULL, &fops);
+	if (!proc_entry) {
+		printk(KERN_ERR "error in creating proc entry\n");
+		goto ERR;
+	}
+	return 0;
+ERR:
+	return -1;
 }
 
-
-static void procex_exit(void)
-{
-   	
-   remove_proc_entry(PROC_NAME,NULL);
-   printk("exiting procex module\n"); 
+static void __exit procex_exit(void)
+{  	
+   remove_proc_entry(PROC_NAME, NULL);
+   printk("exiting procex module\n");
 }
 
 module_init(procex_init);
